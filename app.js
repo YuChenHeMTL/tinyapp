e5684291830cb533834675346dcd63eab37f4b1e
Yu Chen <yuchen7568@gmail.com>
2017-07-04 20:19:40 +0000

    added files

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set('view engine', 'ejs');
+
---
+++ express_server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString(){
+  var finalString = ""
+  finalString = Math.random().toString(36).substr(2, 6);
+  return finalString
+};
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  var randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+  console.log(req.body);
+  res.send("OK");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let shortLink = req.params.shortURL;
+  let longURL = urlDatabase[shortLink];
+  console.log(req.statusMessage)
+  res.redirect(longURL)
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { allURL : urlDatabase, shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
