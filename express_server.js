6aadf6cfdf82d95ee985e495b50144161349c8b8
Yu Chen <yuchen7568@gmail.com>
2017-07-06 03:40:16 +0000

    added GET login

--- express_server.js
+++ express_server.js
@@ -15,6 +15,19 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
 function generateRandomString(){
   var finalString = ""
   finalString = Math.random().toString(36).substr(2, 6);
@@ -27,24 +40,50 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req,res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/")
 })
 
 app.get("/", (req, res) => {
-  let templateVars = {username : req.cookies.username}
+  let templateVars = {user_id : req.cookies.user_id}
   res.render("urls_start", templateVars);
 });
 
-
+app.get("/login", (req, res) => {
+  let templateVars = {user_id: req.cookies.user_id}
+  res.render("urls_login", templateVars);
+})
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies.username};
+  let templateVars = { urls: urlDatabase, user_id: req.cookies.user_id};
   res.render("urls_index", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  let templateVars = {user_id: req.cookies.user_id}
+  res.render("urls_register", templateVars)
+})
+
+app.post("/register", (req, res) => {
+  if (req.body.email==="" || req.body.password===''){
+    console.log(404)
+    res.redirect(404);
+  }
+  for (var user in users){
+    if (req.body.email === users[user].email){
+
+      res.redirect(400);
+    }
+  }
+   var user_id = generateRandomString();
+   res.cookie("user_id", user_id);
+   users[user_id] = {id: user_id, email: req.body.email, password: req.body.password}
+   console.log(users)
+  res.redirect("/")
+})
+
 app.get("/urls/new", (req, res) => {
-  let templateVars = {username : req.cookies.username}
+  let templateVars = {user_id : req.cookies.user_id}
   res.render("urls_new", templateVars);
 });
 
@@ -66,7 +105,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { allURL : urlDatabase, shortURL: req.params.id , username: req.cookies.username};
+  let templateVars = { allURL : urlDatabase, shortURL: req.params.id , user_id: req.cookies.user_id};
   res.render("urls_show", templateVars);
 });
 
